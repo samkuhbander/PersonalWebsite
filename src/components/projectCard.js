11fe4dc3b6fa3fddfe2df44a580ad15c963ea1b5
Sam Kuhbander <kuhbansc@miamioh.edu>
2021-12-25 21:35:35 -0500

    Changed project style

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -5,20 +5,23 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import "../css/projects.css";
 
 const ProjectCard = (props) => {
   return (
-    <Card sx={{ width: 500, m:6}}>
+    <Card sx={{ width: 500, m:5, borderRadius: 4, border: 10, borderColor: "black", transition: "transform 0.15s ease-in-out", "&:hover": { transform: "scale3d(1.10, 1.10, 1)" }}}>
+      <div className="mediaBackground">
       <CardMedia
         component="img"
         alt= {props.alt}
         image= {props.img}
       />
+      </div>
       <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
+        <Typography gutterBottom variant="h3" component="div">
           {props.title}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
+        <Typography variant="h5" color="text.secondary">
           {props.description}
         </Typography>
       </CardContent>
@@ -27,7 +30,7 @@ const ProjectCard = (props) => {
                 rel="noopener noreferrer"
                 target="_blank"
                 style={{ textDecoration: 'none'}}>
-                    <Button variant="outlined" size="large">View</Button>
+                    <Button sx={{ml: 2}} variant="outlined" size="large">View</Button>
                 </a>
       </CardActions>
     </Card>
--- src/projects.js
+++ src/projects.js
@@ -5,30 +5,21 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Reimagine1 from "../src/static/ReimagineCollab1.jpg";
 import MiamiCards1 from "../src/static/MiamiCards1.jpg";
-
+import OffCampus1 from "../src/static/OffCampus1.jpg";
+import Moviefy1 from "../src/static/Moviefy1.jpg";
 
 function Project() {
     return (
         <div>
-            <div style={{textAlign: 'center', padding:40}}>
-                <h1>My Projects</h1>
+            <div style={{textAlign: 'center'}}>
+                <h1 style={{fontSize: 80}}>My Projects</h1>
             </div>
             
             <Grid container>
                 <Grid>
-                    <ProjectCard
-                        title="Reimagine-Collaboration"
-                        description="Created a website for my Lockeed Martin Leadership Institute project. Website uses NodeJS, React, Contentful, and GraphQL.  "
-                        link="https://reimagine-collaboration.com/"
-                        alt="Reimagine Collaboration Webpage"
-                        img= {Reimagine1}
-                    >
-                    </ProjectCard>
-                </Grid>
-                <Grid>
-                    <ProjectCard
+                <ProjectCard
                         title="Miami Cards"
-                        description="Created a web application that allows for the buying and exchange of NFTs. Created using React and Solidity on the Etheruem Blockchain."
+                        description="Created a web application that allows for the buying and exchange of NFTs. Created using React and Solidity on the Etheruem Blockchain the concept was to allow Miami University to create NFTs of their student athletes."
                         link="https://github.com/wsl-miami/miamicards"
                         alt="Miami Cards"
                         img= {MiamiCards1}
@@ -37,21 +28,31 @@ function Project() {
                 </Grid>
                 <Grid>
                     <ProjectCard
-                        title="Title 3"
-                        description="Description text goes here"
-                        link="https://www.instagram.com/samkuhbander"
-                        alt="Alternate text"
-                        img="/static/media/LockheedHeadshot.a1a72983.jpg"
+                        title="Reimagine-Collaboration"
+                        description="Created a website for my Lockeed Martin Leadership Institute Cohort's project Reimagine-Collaboration. This allowed us to post content about the conference and showcase our members. The website uses NodeJS, React, Contentful, and GraphQL.  "
+                        link="https://reimagine-collaboration.com/"
+                        alt="Reimagine Collaboration Webpage"
+                        img= {Reimagine1}
                     >
                     </ProjectCard>
                 </Grid>
                 <Grid>
                     <ProjectCard
-                        title="Title 4"
-                        description="Description text goes here"
-                        link="https://www.instagram.com/samkuhbander"
-                        alt="Alternate text"
-                        img="/static/media/LockheedHeadshot.a1a72983.jpg"
+                        title="Off-Campus"
+                        description="Created a web application for my project management class that showed rental properties around Oxford, Ohio.  The site used was coded in HTML and used a PHP backend to connect to a mySQL database.  It also utilized the google maps api to show property locations. "
+                        link="https://github.com/samkuhbander/OffCampus-ISA-406-"
+                        alt="Off-Campus"
+                        img= {OffCampus1}
+                    >
+                    </ProjectCard>
+                </Grid>
+                <Grid>
+                    <ProjectCard
+                        title="Moviefy"
+                        description="A movie catalog application created for a software engineering class.  I used HTML, Bootstrap, and PHP to create the application and connected to a mySQL database. It allowed users to create and sign into accounts as well view different movies that were listed and leave comments. The web application was hosted using AWS. "
+                        link="https://github.com/jonesn5/Moviefy"
+                        alt="Moviefy"
+                        img= {Moviefy1}
                     >
                     </ProjectCard>
                 </Grid>
